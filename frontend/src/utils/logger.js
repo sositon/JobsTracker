06a176cd73e657f9992f7e96bf377d06c0912104
Omer Siton <omersiton11@gmail.com>
2024-09-10 16:37:36 +0300

    logging with pino

--- frontend/src/utils/logger.js
+++ frontend/src/utils/logger.js
@@ -2,6 +2,10 @@ import pino from 'pino';
 
 export const logger = pino({
   level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+	prettyPrint: process.env.NODE_ENV !== 'production' ? {
+		colorize: true,
+		translateTime: 'SYS:standard',
+	} : false,
   transport: {
     target: 'pino-pretty'
   },
